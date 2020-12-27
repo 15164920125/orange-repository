3ce24f1f04a16597e0ce033be819f26353b151af
15164920125 <15164920125@163.com>
2020-12-27 11:50:14 +0800

    添加忽略配置

--- OrangeP1/src/main/java/com/orange/business/controller/TestRequestController.java
+++ OrangeP1/src/main/java/com/orange/business/controller/TestRequestController.java
@@ -1,10 +1,6 @@
 package com.orange.business.controller;
 
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withUnauthorizedRequest;
-
-import java.util.Random;
 import java.util.UUID;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
